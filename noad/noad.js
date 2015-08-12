04a3d96e9c443d226cde30c3b9604c2032baa445
chinatone <china.tone@msa.hinet.net>
2015-08-12 12:25:39 +0800

    First ver.

---
+++ noad/noad.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+try{
+var _hinet = parent.document.getElementsByTagName("frameset");
+for(var i=0;i<_hinet.length;i++){_hinet[i].cols="*,0";}}
+catch(err){};
+AdPageURL=null;
+Reload_AD_Page=null;
+</script>
